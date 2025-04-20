3cc140adf1b0f6238afe20094cdca4088873f821
Dulce Figueredo <dulcefigueredo2252@gmail.com>
2025-04-20 17:03:47 -0300

    basic JavaScript and the TypeWriter plugin were added

---
+++ src/script.js
@@ -0,0 +1,15 @@
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: [
+      "In the hush of morning light, Dreams take wing and hearts feel right.",
+    ],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
